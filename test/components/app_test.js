19b9758ddc8fd6e316e9f234e8babba84b4052fd
ssma100701 <ssma100701@gmail.com>
2018-03-06 13:56:29 +1100

    lesson 1

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,17 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use 'describe' to group together similar tests
+describe('App', () => {
+  
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+    
+    // Use 'expect' to make an 'assertion' about a target
+    expect
 
-  beforeEach(() => {
-    component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
+
+
